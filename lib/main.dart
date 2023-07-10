3cd6408dd3df630e0af5809e52e1956f2f2c05ab
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-07-10 23:37:03 +0530

    Refactored the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:file_manager/InitialPage.dart';
 import 'package:file_manager/SecondPage.dart';
 void main() {
